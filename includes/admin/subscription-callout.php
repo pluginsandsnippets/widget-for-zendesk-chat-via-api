f1eaea2c82efe263764a9f9a040d1ae908ba7030
Owais <siawa.ahmed90@gmail.com>
2022-11-21 14:13:08 +0500

    #17 Fix Language Domain in Subscription Callout

--- includes/admin/subscription-callout.php
+++ includes/admin/subscription-callout.php
@@ -8,22 +8,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="ps-wfzcva-subscription-callout-wrapper">
 	<div class="ps-wfzcva-subscription-callout">
 		<div class="ps-wfzcva-subscription-callout-main">
-			<h3><?php _e( 'Subscribe to our Newsletter', 'ps-plugin-template' ); ?></h3>
-			<p><?php _e( 'Receive updates from <a href="https://www.pluginsandsnippets.com" target="_blank">Plugins & Snippets</a> with respect to WordPress plugins aimed to enhance the conversion rates of your web stores.', 'ps-plugin-template' ); ?></p>
+			<h3><?php _e( 'Subscribe to our Newsletter', 'widget-for-zendesk-chat-via-api' ); ?></h3>
+			<p><?php _e( 'Receive updates from <a href="https://www.pluginsandsnippets.com" target="_blank">Plugins & Snippets</a> with respect to WordPress plugins aimed to enhance the conversion rates of your web stores.', 'widget-for-zendesk-chat-via-api' ); ?></p>
 
-			<div class="ps-wfzcva-subscription-error" style="display: none;"><?php _e( 'There was an error in processing your request, please try again.', 'ps-plugin-template' ); ?></div>
+			<div class="ps-wfzcva-subscription-error" style="display: none;"><?php _e( 'There was an error in processing your request, please try again.', 'widget-for-zendesk-chat-via-api' ); ?></div>
 
 			<form method="POST" class="ps-wfzcva-subscription-form">
 				<input type="email" required value="<?php echo esc_attr( get_option( 'admin_email' ) ); ?>">
 				
 				<div class="ps-wfzcva-subscription-actions">
-					<button class="button-primary"><?php _e( 'Subscribe', 'ps-plugin-template' ); ?></button>
+					<button class="button-primary"><?php _e( 'Subscribe', 'widget-for-zendesk-chat-via-api' ); ?></button>
 				</div>
 			</form>
 		</div>
 
 		<div class="ps-wfzcva-subscription-callout-thanks" style="display: none;">
-			<h3><?php _e( 'Thank you for signing up to our Newsletter!', 'ps-plugin-template' ); ?></h3>
+			<h3><?php _e( 'Thank you for signing up to our Newsletter!', 'widget-for-zendesk-chat-via-api' ); ?></h3>
 		</div>
 
 	</div>
